9d61e10189b29b1edb351badd586974fdd2f4c89
KevinNaserwan <kevinnaserwan@gmail.com>
2022-12-02 18:37:47 +0700

    Portfolio Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '2px',
+    },
+    extend: {},
+    colors: {
+      primary: '#0284c7',
+      dark: '#0f172a',
+      secondary: '#94a3b8',
+      white:'#f8fafc',
+      border:'#cbd5e1',
+      portfolio: '#f1f5f9',
+      client:'#1e293b',
+      contact: '#e2e8f0',
+      akhir: '#64748b',
+      wt:'#0ea5e9'
+    },
+  },
+  plugins: [],
+}
